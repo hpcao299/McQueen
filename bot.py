1ad5698976a1ab44e0ec8a74e2b6b402fd1fd67e
HPhucJ2007 <yourgitemail@example.com>
2024-04-25 15:13:10 +0700

    get ip commands

--- bot.py
+++ bot.py
@@ -2,10 +2,25 @@ import socket
 import os
 from requests import get
 import telebot
+import validators
+from urllib.parse import urlparse
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
 bot = telebot.TeleBot(BOT_TOKEN)
+
+def get_ip_details(ip = ""):
+    if ip:
+        print('Params ip: ' + ip)
+        response = get(f'https://ipinfo.io/{ip}/json')
+        ip_details = response.json()
+
+        return f"Public IPv4: {ip_details['ip']}\nLocation: {ip_details['city']}/{ip_details['country']}\nTimezone: {ip_details['timezone']}\nHostname: {ip_details['hostname']}\nOrigin: {ip_details['org']}"
+    else:
+        response = get('https://ipinfo.io/json')
+        ip_details = response.json()
+
+        return f"Public IPv4: {ip_details['ip']}\nLocation: {ip_details['city']}/{ip_details['country']}\nTimezone: {ip_details['timezone']}\nHostname: {ip_details['hostname']}\nOrigin: {ip_details['org']}"        
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -13,12 +28,32 @@ def send_welcome(message):
 
 @bot.message_handler(commands=['myip'])
 def get_my_ip(message):
-    public_ipv6 = get('https://api6.ipify.org').text
-    response = get('https://ipinfo.io/json')
+    try:
+        public_ipv6 = get('https://api6.ipify.org').text
+        ip_details = get_ip_details()
 
-    ip_details = response.json()
-    bot.send_message(chat_id=message.chat.id, text=f"Public IPv4: {ip_details['ip']}\nPublic IPv6: {public_ipv6}\nLocation: {ip_details['city']}/{ip_details['country']}\nTimezone: {ip_details['timezone']}\nHostname: {ip_details['hostname']}\nOrigin: {ip_details['org']}")
+        bot.send_message(chat_id=message.chat.id, text=f"Public IPv6: {public_ipv6}\n{ip_details}")
+    except Exception:
+        bot.send_message(chat_id=message.chat.id, text="There is something wrong in our server")
 
+@bot.message_handler(commands=['trackwebsite'])
+def get_website_ip(message):
+    text = message.text
+    content = text.split("/trackwebsite", 1)[1].strip()
 
+    if validators.url(content):
+        try:
+            # Fix error with some specific ip address
+            hostname = urlparse(content).netloc
+            ip = socket.gethostbyname(hostname)
+            ip_details = get_ip_details(ip)
+            print(ip_details)
+            bot.send_message(chat_id=message.chat.id, text=f"Result for {content}\n\n{ip_details}")
+        except Exception as e:
+            print("Error resolving hostname:", e)
+            bot.send_message(chat_id=message.chat.id, text="There is something wrong in our server.")
+    else:
+        bot.send_message(chat_id=message.chat.id, text="Given URL is invalid.")
+        
 
 bot.polling()
