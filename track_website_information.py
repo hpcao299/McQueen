017b30871b76b58bd9e3b0551a2fd3fdc50d2038
hpcao299 <caohoangphuc2909@gmail.com>
2024-04-29 23:16:11 +0700

    add name server details to returned website information

--- track_website_information.py
+++ track_website_information.py
@@ -1,9 +1,18 @@
 import socket
 from requests import get
 from urllib.parse import urlparse
+import dns.resolver
+
+def get_ip_details(ip):
+    response = get(f'https://ipinfo.io/{ip}/json')
+    ip_details = response.json()
+    
+    return ip_details
 
 def run():
     try:
+        resolver = dns.resolver.Resolver()
+
         # url = input('Enter website url (https://example.com): ')
         # parsed_url = urlparse(url)
         # hostname = parsed_url.netloc
@@ -14,22 +23,21 @@ def run():
         ipv4_addresses = [addr for addr in ip_addresses if ":" not in addr]
         ipv6_addresses = [addr for addr in ip_addresses if ":" in addr]
 
-        def get_ip_details(hostname):
-            ip = socket.gethostbyname(hostname)
-            response = get(f'https://ipinfo.io/{ip}/json')
-            ip_details = response.json()
-            
-            return ip_details
-            
-        
+        ip = socket.gethostbyname(hostname)
+
+        nameservers = resolver.resolve(hostname, 'NS')
+
         print('IPv4 Address:')
         for ip in ipv4_addresses:
             print(f"   - {ip}")
         print('IPv6 Address:')
         for ip in ipv6_addresses:
             print(f"   - {ip}")
+        print('Nameservers:')
+        for nameserver in nameservers:
+            print(f"   - {nameserver}")
 
-        ip_details = get_ip_details(hostname)
+        ip_details = get_ip_details(ip)
 
         print(f"Country: {ip_details['country']}")
         print(f"Region: {ip_details['region']}")
@@ -38,6 +46,12 @@ def run():
         print(f"Timezone: {ip_details['timezone']}")
         print(f"Origin: {ip_details['org']}")
         print(f"Postal: {ip_details['postal']}")
+
+    except dns.resolver.NoAnswer as e:
+        print(e)
+        print("\nNo answers from DNS resolver.")
     except Exception as e:
         print(e)
-        print('\nExited!')
+        print('\nThere is something wrong. Exited!')
+
+run()
