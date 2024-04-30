ff983c31c3d291f0f6cc005c482b7b79004b3160
hpcao299 <caohoangphuc2909@gmail.com>
2024-04-30 14:03:54 +0700

    check domain registrant function

---
+++ check_domain_registrant.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+from requests import get
+
+def run():
+    # hostname = input('Enter a domain (e.g: example.com): ')
+    hostname = 'saigonnguyen.online'
+
+    whois_link = "https://who.is/whois/"
+
+    print('Searching domain registrant...')
+    
+    res = get(f"{whois_link}{hostname}").text
+    doc = BeautifulSoup(res, "html.parser")
+
+    registrant_strong = doc.find('strong', text='Registrant Contact Information:')
+    info_wrapper = registrant_strong.parent.find_next_sibling('div')
+
+    updated_divs = doc.select(selector='div:-soup-contains("Information Updated:")')
+    updated_info = updated_divs[len(updated_divs) - 1]
+
+    info_rows = info_wrapper.find_all('div', class_ = "row")
+
+    print(f'\n{hostname} registrant info:')
+    for row in info_rows:
+        key_element = row.find('strong')
+        value_element = key_element.parent.find_next_sibling('div')
+        print(f'- {key_element.text}: {value_element.text}') if bool(value_element.text) else print(f'- {key_element.text}: None')
+
+    info = updated_info.text.split(': ')
+    print('\nInformation updated: {date_updated}'.format(date_updated=info[1]))
